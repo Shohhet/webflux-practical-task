bcab1c0d340a25e0131a9e54fff22904e0170751
shohhet <shohhet@gmail.com>
2024-03-16 22:41:42 +0700

    delete unused import

--- src/main/java/com/shoggoth/webfluxfileserver/security/token/TokenConverter.java
+++ src/main/java/com/shoggoth/webfluxfileserver/security/token/TokenConverter.java
@@ -1,6 +1,5 @@
 package com.shoggoth.webfluxfileserver.security.token;
 
-import com.shoggoth.webfluxfileserver.security.token.Token;
 
 public interface TokenConverter {
     String convert(Token token);
